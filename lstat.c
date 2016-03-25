eecfea71604bb05a49777bbe119f58ce0ee61ccf
Joshua Abraham <joshuaabraham@Joshuas-MacBook-Air.local>
2016-03-25 17:42:07 -0400

    Formatting

--- lstat.c
+++ lstat.c
@@ -8,11 +8,12 @@ creation time
 
 */
 
+
 #include <stdio.h>
-#include <fcntl.h>
-#include <sys/types.h>
+#include <fcntl.h>     
+#include <sys/types.h>     
 #include <dirent.h>
-#include <sys/stat.h>
+#include <sys/stat.h>     
 #include <time.h>
 
 int main(int argc, char* argv[])
@@ -25,21 +26,22 @@ int main(int argc, char* argv[])
 
         struct dirent* dir_element;
         struct stat file_info;
-	time_t c;
-	time_t m;
-	time_t cr;
+        time_t c;
+        time_t m;
+        time_t cr;
 
         for (int i = 1; i < argc; i++)
         {
-		//lstat(dir_element->d_name, &file_info);
-		lstat( argv[i], &file_info );
-		c = (long long)file_info.st_atim.tv_sec;
-		m = (long long)file_info.st_mtim.tv_sec;
-		cr = (long long)file_info.st_ctim.tv_sec;
-		printf( ctime( &c ) );
-		printf( ctime( &m ) );
-		printf( ctime( &cr ) );
-		//printf("%lld.%.9ld\n", (long long)file_info.st_atim.tv_sec, file_info.st_atim.tv_nsec);
+                //lstat(dir_element->d_name, &file_info);
+                printf("%s\n", argv[i]);
+                lstat( argv[i], &file_info );
+                c = (long long)file_info.st_atim.tv_sec;
+                m = (long long)file_info.st_mtim.tv_sec;
+                cr = (long long)file_info.st_ctim.tv_sec;
+                printf("Create time: %s", ctime( &cr ) );
+                printf("Access time: %s", ctime( &c ) );
+                printf("Modification time: %s", ctime( &m ) );
+                                //printf("%lld.%.9ld\n", (long long)file_info.st_atim.tv_sec, file_info.st_atim.tv_nsec);
         }
         return 0;
 }
