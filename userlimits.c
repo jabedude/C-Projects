4146885a5f0fb8d71e70b95fa5fd70bda41deede
Joshua Abraham <sinisterpatrician@gmail.com>
2016-04-09 23:45:45 -0400

    using BSD/Linux getrlimit to find stack limits

---
+++ userlimits.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <sys/resource.h>
+#include <stdint.h>
+
+int main(int argc, char* argv[])
+{
+	struct rlimit rlim;
+	int64_t cur;
+	int64_t max;
+	
+	getrlimit(RLIMIT_STACK, &rlim);
+	cur = rlim.rlim_cur;
+	max = rlim.rlim_max;
+	
+	printf("Hard stack limit: %#llx\nCurrent Stack Limit: %#llx\n", max, cur);
+
+	return 0;
+}
