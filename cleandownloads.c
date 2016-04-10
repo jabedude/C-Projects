448d17716298aa106e916850906f4d3656e015bf
Joshua Abraham <sinisterpatrician@gmail.com>
2016-04-10 19:01:21 -0400

    description

--- cleandownloads.c
+++ cleandownloads.c
@@ -1,3 +1,9 @@
+/*
+
+Deletes mp3 and gif files in a directory 
+
+*/
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <dirent.h>
@@ -5,9 +11,6 @@
 
 //Credit:
 //http://stackoverflow.com/questions/12489/how-do-you-get-a-directory-listing-in-c
-
-int fileIsMp(char* s);
-
 int main(int argc, char* argv[])
 {
 	DIR* targetDir;
@@ -46,21 +49,3 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
-/*
-int fileIsMp(char* s)
-{
-	char s[] = ep->d_name;
-	size_t size = strlen(s);
-	
-	if (s[size-4] == '.' &&
-	    s[size-3] == 'm' &&
-	    s[size-2] == 'p' &&
-	    s[size-1] == '3')
-	{
-		remove(s);
-	}
-	else
-		perror("Couldn't delete file :(");
-}
-*/
-
